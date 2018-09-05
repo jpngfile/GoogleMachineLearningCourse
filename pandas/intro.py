f40ae1c23fdb7a4ce1a34a96b76cbf2489615565
Jason P'ng <jason_p'ng@ultimatesoftware.com>
2018-09-05 15:34:39 -0700

    Added a few basic pandas data summaries

--- pandas/intro.py
+++ pandas/intro.py
@@ -1,3 +1,14 @@
 from __future__ import print_function
 import pandas as pd
 print(pd.__version__)
+
+city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento'])
+population = pd.Series([852469, 1015785, 485199])
+
+popData = pd.DataFrame({ 'City name': city_names, 'Population': population })
+print(popData)
+
+california_housing_dataframe = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
+print(california_housing_dataframe.describe())
+print(california_housing_dataframe.head())
+hist = california_housing_dataframe.hist('housing_median_age')
